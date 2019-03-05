ce6338a3283a476bd036b080f4645f2a4a697e6a
Paweł Kowalski <pavelloz@gmail.com>
2019-03-05 14:08:11 +0100

    Add info type bootstrap alert

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ import { Selector } from 'testcafe';
 import { checkLiquidErrors, getResultElement, getResultText, getBtAlertElement, getBtAlertText } from './index';
 
 const url = 'https://examples.staging.oregon.platform-os.com/multilanguage';
-const forceAlertsQuery = 'flash[notice]=Flash%20notice&flash[error]=Flash%20error&flash[alert]=Flash%20alert';
+const forceAlertsQuery =
+  'flash[notice]=Flash%20notice&flash[error]=Flash%20error&flash[alert]=Flash%20alert&flash[info]=Flash%20info';
 
 fixture('Test helpers').page(url);
 
@@ -29,15 +30,19 @@ test('getBtAlertElement', async t => {
   const dangerElement = await getBtAlertElement({ type: 'danger', Selector });
   const warningElement = await getBtAlertElement({ type: 'warning', Selector });
   const successElement = await getBtAlertElement({ type: 'success', Selector });
+  const infoElement = await getBtAlertElement({ type: 'info', Selector });
+  const noTypeElement = await getBtAlertElement({ type: 'success', Selector });
 
   const dangerText = await dangerElement.innerText;
   const warningText = await warningElement.innerText;
   const successText = await successElement.innerText;
-  const noTypeText = await successElement.innerText;
+  const infoText = await infoElement.innerText;
+  const noTypeText = await noTypeElement.innerText;
 
   await t.expect(dangerText).contains('Flash error');
   await t.expect(warningText).contains('Flash alert');
   await t.expect(successText).contains('Flash notice');
+  await t.expect(infoText).contains('Flash info');
   await t.expect(noTypeText).contains('Flash notice');
 });
 
@@ -47,10 +52,12 @@ test('getBtAlertText', async t => {
   const danger = await getBtAlertText({ type: 'danger', Selector });
   const warning = await getBtAlertText({ type: 'warning', Selector });
   const success = await getBtAlertText({ type: 'success', Selector });
+  const info = await getBtAlertText({ type: 'info', Selector });
   const noType = await getBtAlertText({ type: 'success', Selector });
 
   await t.expect(danger).contains('Flash error');
   await t.expect(warning).contains('Flash alert');
   await t.expect(success).contains('Flash notice');
+  await t.expect(info).contains('Flash info');
   await t.expect(noType).contains('Flash notice');
 });
